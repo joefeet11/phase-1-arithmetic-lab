1e7e876b7d057b5d83ce855d5875de8105054cf4
Kevin Olivier <kevinolivier1111@gmail.com>
2022-09-20 09:31:37 -0600

    commit tree 8fb0c656666ebe4989265ce96387c6b2b3470d4b

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31 
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random()*100) +1;
+
+const num3 = 104
+const num4 = 25
+
+let mod = num3 % num4;
+let max
+function maximum () {
+
+    max= Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20);
+    return max;
+};
+
+maximum()
+
+
